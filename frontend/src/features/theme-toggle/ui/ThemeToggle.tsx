0a6d49974b653b5ec68d87b9fb6259aadbb66b8b
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2026-03-08 05:14:58 +0500

    Исправлена предупреждения из серверного рендеринга по поводу кнопки изменения темы

--- frontend/src/features/theme-toggle/ui/ThemeToggle.tsx
+++ frontend/src/features/theme-toggle/ui/ThemeToggle.tsx
@@ -1,10 +1,27 @@
+/* eslint-disable react-hooks/set-state-in-effect */
 "use client";
 
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
+import { useLayoutEffect, useState } from "react";
 
 export const ThemeToggle = () => {
   const { theme, setTheme } = useTheme();
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+
+  useLayoutEffect(() => setIsMounted(true), []);
+
+  if (!isMounted)
+    return (
+      <button
+        type="button"
+        className="fixed top-5 right-5"
+        aria-hidden="true"
+        disabled
+      >
+        <div className="w-6 h-6" />
+      </button>
+    );
 
   return (
     <button
