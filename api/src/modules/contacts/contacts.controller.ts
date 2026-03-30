93ca7e56c4aa8be05d5e21b5f311dee0b1ec616b
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2026-03-30 22:18:04 +0500

    Вынесен search из user в contacts и исправлен тип на такой же как и у get-all

--- api/src/modules/contacts/contacts.controller.ts
+++ api/src/modules/contacts/contacts.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Delete, Get, HttpCode, Post, UseGuards } from "@nestjs/common";
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	Post,
+	Query,
+	UseGuards,
+} from "@nestjs/common";
 import { ContactsService } from "./contacts.service";
 import { ContactsRepository } from "./contacts.repository";
 import { CurrentUser } from "@/app/common/decorators/current-user.decorator";
@@ -12,39 +21,46 @@ export class ContactsController {
 		private readonly contactsRepository: ContactsRepository,
 	) {}
 
-  @Get("")
-  @HttpCode(200)
-  async getContacts(
-    @CurrentUser("id") userId: string
-  ) {
-    const contacts = await this.contactsRepository.getUserContacts(userId);
+	@Get("")
+	@HttpCode(200)
+	async getContacts(@CurrentUser("id") userId: string) {
+		const contacts = await this.contactsRepository.getUserContacts(userId);
 
-    return contacts;
-  }
+		return contacts;
+	}
 
-  @Post("")
-  @HttpCode(201)
-  async addContact(
-    @CurrentUser("id") userId: string,
-    @Body("contactId") contactId: string
-  ) {
-    await this.contactsService.addContact(userId, contactId);
-    
-    return {
-      message: "Контакт успешно добавлен",
-    }
-  }
+	@Get("search")
+	@HttpCode(200)
+	async search(
+		@CurrentUser("id") userId: string,
+		@Query("query") search: string,
+	) {
+		return this.contactsRepository.searchContact(userId, search);
+	}
 
-  @Delete("")
-  @HttpCode(200)
-  async removeContact(
-    @CurrentUser("id") userId: string,
-    @Body("contactId") contactId: string
-  ) {
-    await this.contactsService.removeContact(userId, contactId);
+	@Post("")
+	@HttpCode(201)
+	async addContact(
+		@CurrentUser("id") userId: string,
+		@Body("contactId") contactId: string,
+	) {
+		await this.contactsService.addContact(userId, contactId);
 
-    return {
-      message: "Контакт успешно удален",
-    }
-  }
+		return {
+			message: "Контакт успешно добавлен",
+		};
+	}
+
+	@Delete("")
+	@HttpCode(200)
+	async removeContact(
+		@CurrentUser("id") userId: string,
+		@Body("contactId") contactId: string,
+	) {
+		await this.contactsService.removeContact(userId, contactId);
+
+		return {
+			message: "Контакт успешно удален",
+		};
+	}
 }
--- api/src/modules/contacts/contacts.repository.ts
+++ api/src/modules/contacts/contacts.repository.ts
@@ -1,5 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
+import { Contact } from "@/app/generated/prisma/client";
+import { QueryMode } from "@/app/generated/prisma/internal/prismaNamespace";
 
 @Injectable()
 export class ContactsRepository {
@@ -17,5 +19,43 @@ export class ContactsRepository {
 				},
 			},
 		});
+	}
+
+	async searchContact(userId: string, search: string): Promise<Contact[] | []> {
+		if (!search.trim()) return [];
+
+		const contacts = await this.prismaService.client.contact.findMany({
+			where: {
+				ownerId: userId,
+				contactId: { not: userId },
+				contact: {
+					OR: [
+						{
+							name: {
+								contains: search.trim(),
+								mode: QueryMode.insensitive,
+							},
+						},
+						{
+							username: {
+								contains: search.trim(),
+								mode: QueryMode.insensitive,
+							},
+						},
+					],
+				},
+			},
+			take: 20,
+			include: {
+				owner: {
+					include: { chatMembers: { where: { chat: { type: "DIRECT" } } } },
+				},
+				contact: {
+					include: { chatMembers: { where: { chat: { type: "DIRECT" } } } },
+				},
+			},
+		});
+
+		return contacts;
 	}
 }
